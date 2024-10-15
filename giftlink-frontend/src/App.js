c249cbf456d63a79bb001067e16f2671111e4126
abdelilahsaid01 <abdelilahsaid01@gmail.com>
2024-10-15 06:20:48 -0400

    update resgiterPage component

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -9,19 +9,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 function App() {
-  const navigate = useNavigate();
-  return (
+    const navigate = useNavigate();
+    return (
         <>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-          <Route path="/app/product/:productId" element={<DetailsPage/>} />
-          <Route path="/app/search" element={<SearchPage/>} />
-        </Routes>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/app" element={<MainPage />} />
+                <Route path="/app/login" element={<LoginPage />} />
+                <Route path="/app/register" element={<RegisterPage />} />
+                <Route path="/app/product/:productId" element={<DetailsPage />} />
+                <Route path="/app/search" element={<SearchPage />} />
+            </Routes>
         </>
-  );
+    );
 }
 export default App;
